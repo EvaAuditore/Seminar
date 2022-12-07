4f2bbb9f42e4f6255c027ddb9356ec507f396250
Eva_Auditore <Murzina.T.N@yandex.ru>
2022-12-07 15:51:26 +0700

    добавлены исправления

--- DZ/4_lession/task_2.py
+++ DZ/4_lession/task_2.py
@@ -5,8 +5,12 @@
 # [1,1,1,1,2,2,2,3,3,3,4] -> [1,2,3,4]
 
 list_number = [1,1,1,1,2,2,2,3,3,3,4]
-list_sort_number = []
-for i in list_number:
-    if i not in list_sort_number:
-        list_sort_number.append(i)
-print(list_sort_number)
+
+def sort_number(list_number):
+    list_sort_number = []
+    for i in list_number:
+        if i not in list_sort_number:
+            list_sort_number.append(i)
+    print(list_sort_number)
+
+sort_number(list_number)
